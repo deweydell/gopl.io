03dfeb8781317a2b078134d8ae9e3f5b217b2f5c
Nicola Beuscher <nbeuscher@gmail.com>
2018-09-26 15:54:48 -0400

    Add comments to dup1

--- ch1/dup1/main.go
+++ ch1/dup1/main.go
@@ -15,14 +15,20 @@ import (
 )
 
 func main() {
+	// Create map of string/int combinations
 	counts := make(map[string]int)
+	// Read input & break it into lines/workds
 	input := bufio.NewScanner(os.Stdin)
+	// Reads next line and removes newline character, result is input.Text()
+	// returns true if there is a line and false where no more input
 	for input.Scan() {
 		counts[input.Text()]++
 	}
 	// NOTE: ignoring potential errors from input.Err()
 	for line, n := range counts {
 		if n > 1 {
+			// Returns formatted output from a list
+			// First argument is a format string  using conversion characters (letter%)
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
